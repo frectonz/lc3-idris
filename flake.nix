f3cc707d8723bea71c4aa60ea22c4b75dd26913b
frectonz <fraol0912@gmail.com>
2025-05-14 19:33:18 +0300

    chore: init project

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      forAllSystems =
+        fn:
+        let
+          systems = [
+            "x86_64-linux"
+            "aarch64-darwin"
+          ];
+          overlays = [ ];
+        in
+        nixpkgs.lib.genAttrs systems (
+          system:
+          fn (
+            import nixpkgs {
+              inherit system overlays;
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.idris2
+            pkgs.asciinema
+            pkgs.idris2Packages.idris2Lsp
+          ];
+        };
+      });
+    };
+}
---
+++ src/lc3.idr
@@ -0,0 +1,5 @@
+module Lc3
+
+main : IO ()
+main = do
+  putStrLn "Hello There"
