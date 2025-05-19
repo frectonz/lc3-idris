2c7ad88edd2aba04b36d4e63be25fc293232f660
frectonz <fraol0912@gmail.com>
2025-05-19 20:18:00 +0300

    feat: build project with nix

--- flake.nix
+++ flake.nix
@@ -1,40 +1,29 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-    }:
+  outputs = { self, nixpkgs, }:
     let
-      forAllSystems =
-        fn:
+      forAllSystems = fn:
         let
-          systems = [
-            "x86_64-linux"
-            "aarch64-darwin"
-          ];
+          systems = [ "x86_64-linux" "aarch64-darwin" ];
           overlays = [ ];
-        in
-        nixpkgs.lib.genAttrs systems (
-          system:
-          fn (
-            import nixpkgs {
-              inherit system overlays;
-            }
-          )
-        );
-    in
-    {
+        in nixpkgs.lib.genAttrs systems
+        (system: fn (import nixpkgs { inherit system overlays; }));
+    in {
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
-          packages = [
-            pkgs.idris2
-            pkgs.asciinema_3
-          ];
+          packages = [ pkgs.idris2 pkgs.asciinema_3 pkgs.gmp ];
         };
       });
+
+      packages = forAllSystems (pkgs:
+        let
+          lc3 = pkgs.idris2Packages.buildIdris {
+            ipkgName = "lc3";
+            src = ./.;
+            version = "0.0.1";
+            idrisLibraries = [ ];
+          };
+        in { default = lc3.executable; });
     };
 }
